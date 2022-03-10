995fb2713b096bd27e12742ea465363a25aff379
Jaek1783 <73649967+Jaek1783@users.noreply.github.com>
2022-03-10 14:18:29 +0900

    빵쌤 원페이지 포트폴리오 완성본

--- index.js
+++ index.js
@@ -6,61 +6,74 @@ function slider(){
       currentIndex = 0,
       topHeight = 0,
       timer = undefined,
+      pagerHTML ='',
+      pager = document.querySelector('.pager'),
+      // pagerBtn = document.querySelectorAll('.pager span'),
       navPrev = document.getElementById('prev'),
       navNext = document.getElementById('next');
+      var slide = document.querySelector('.slides'),
+      slideAll = document.querySelectorAll('.slides li'),
+      currentIndex = 0,
+      slideAllCount = slideAll.length,
+      slideWidth = 100 + '%',
+      slideMargin = 0;
 
       console.log(slideCount);
       // console.log(slideCount);
-      // 슬라이더의 높이구하기
-      topHeight = slides[0].offsetHeight;
-      console.log(topHeight);
-      // sliderWrapper[0].style.height = topHeight + 'px';
-      sliderContainer[0].style.height = topHeight + 'px';
-      //슬라이드 가로로 배열하기
-      makeClone();
-      function makeClone(){
+  //슬라이드 가로로 배열하기
         for(var i = 0;i < slideCount;i++){
           slides[i].style.left = i*100 + '%';
-          var cloneSlide = slides[i].cloneNode(true);
-          cloneSlide.classList.add('clone');
-          sliderContainer[0].appendChild(cloneSlide);
+          pagerHTML += '<span data-idx="'+ i +'">'+(i+1)+'</span>';
+          pager.innerHTML = pagerHTML;
         }
-      }
+      var pagerBtn = document.querySelectorAll('.pager span');
+          // <span data-idx="0">1</span>
+  //슬라이드 이동 함수
     function goToSlide(idx){
     sliderContainer[0].classList.add('animated');
     sliderContainer[0].style.left = idx * -100 + '%';
     currentIndex = idx;
+    console.log(currentIndex);
+  // 클릭된 그 요소에만 .active 추가
+    for(var y=0; y < pagerBtn.length; y++){
+      pagerBtn[y].classList.remove('active');
+    }
+      pagerBtn[idx].classList.add('active');
   }
-    // 버튼 클릭시
-      navPrev.addEventListener('click',function(ev){
-        navNext.classList.remove('click')
-        this.classList.add('click');
-        ev.preventDefault();
-        goToSlide(currentIndex - 1);
-      });
-      navNext.addEventListener('click',function(ev){
-        navPrev.classList.remove('click')
-        this.classList.add('click');
-        ev.preventDefault();
-        goToSlide(currentIndex + 1);
-      });
       goToSlide(0);
 
-      // 자동 슬라이드
+  // 자동 슬라이드
       function slideAuto(){
         timer = setInterval(function(){
             var nextIdx = (currentIndex + 1) % slideCount;
 
             goToSlide(nextIdx);
 
-          },4000);
+          },6000);
       }
       slideAuto();
-// // 마우스 할 일
-//       sliderWrapper[0].addEventListener('mouseenter',function(){
-//         clearInterval(timer);
-//       });
-//       sliderWrapper[0].addEventListener('mouseleave',function(){
-//         slideAuto();
-//       });
+  // pager로 슬라이드 이동하기
+  for(var x = 0; x < pagerBtn.length;x++){
+    pagerBtn[x].addEventListener('click',function(event){
+      console.log(event.target.innerText);
+// innerText 요소의내용 반환 A.innerText / A.innerText='B';
+// innertHTML 요소의 태그 반환 A.innerHTMl / A.innerHTML='B';
+// var pagerNum = event.target.getAttribute('data-idx');
+      var pagerNum = event.target.innerText-1;
+      goToSlide(pagerNum);
+    });
+  }
+}
+function gnb(){
+  var sGnb = document.getElementsByClassName('sGnb');
+  var gnb = document.querySelector('.gnb');
+  sGnb[0].classList.add('animated');
+  sGnb[0].style.left = 100 + '%';
+  gnb.addEventListener('click',function(){
+    sGnb[0].style.left = 0 ;
+  });
+  var close = document.querySelector('.close');
+  close.addEventListener('click',function(){
+    sGnb[0].style.left = 100 + '%';
+  });
 }
